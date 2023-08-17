60b79b669110c47c3fec310b9d45579348b5d001
Yasmin <yka@luftborn.com>
2023-08-17 14:23:10 +0300

    make search appear in small pages

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,10 +8,14 @@ export default function Navbar() {
   return (
 <nav className="bg-white border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="https://github.com/yasminkasem66" className="flex items-center">
+    <a href="https://github.com/yasminkasem66" className="flex items-center ">
         <Image src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Jasmin Logo" width={50} height={50} />
         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Jasmin</span>
     </a>
+    <div className='md:hidden block  w-full  mt-4 md:mt-0 '>
+    <Search></Search>
+
+    </div>
 
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul className="font-medium flex flex-col   md:items-center p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
